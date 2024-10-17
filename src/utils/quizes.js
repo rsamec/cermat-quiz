aad3499e6fa8b2a5b905020f0eea8b00be7d5032
rsamec <roman.samec2@gmail.com>
2024-10-17 21:38:14 +0200

    remove missing tests

--- src/utils/quizes.js
+++ src/utils/quizes.js
@@ -4,13 +4,13 @@ const generateCode = (code, variants ) =>
 export const quizes = [
   { subject: 'en', period: 'diploma', codes: ["AJA-2023", "AJB-2023", "AJA-2024", "AJB-2024"] },
   { subject: 'de', period: 'diploma', codes: ["DEA-2023"] },
-  { subject: 'cz', period: '8', codes:generateCode("C5",["A","B"])},
-  { subject: 'cz', period: '4', codes:generateCode("C9",["A","B","C","D"])},
-  { subject: 'cz', period: '6', codes:generateCode("C7",["A","B"])},
+  { subject: 'cz', period: '8', codes:generateCode("C5",["A"]).concat("C5B-2023")},
+  { subject: 'cz', period: '4', codes:generateCode("C9",["A","B"]).concat("C9C-2023")},
+  { subject: 'cz', period: '6', codes:generateCode("C7",["A"])},
   { subject: 'cz', period: 'diploma', codes:generateCode("CM",["A","B"])},
-  { subject: 'math', period: '8', codes:generateCode("M5",["A","B"])},
+  { subject: 'math', period: '8', codes:generateCode("M5",["A"])},
   { subject: 'math', period: '4', codes:generateCode("M9",["A","B","C","D"])},
-  { subject: 'math', period: '6', codes:generateCode("M7",["A","B"])},
+  { subject: 'math', period: '6', codes:generateCode("M7",["A"])},
   //{ subject: 'math', period: 'diploma', codes:[]},
 ]
 
