210e1968279496ea8956203bc50750208d621dda
rsamec <roman.samec2@gmail.com>
2026-04-11 12:46:33 +0200

    fix build + update pdfs

--- run-pdf.js
+++ run-pdf.js
@@ -2,10 +2,10 @@
 import { execSync } from "node:child_process";
 
 const subjects = ["math", "cz", "en", "de"];
-const allPeriods = ['4','6', '8', 'diploma']
+const allPeriods = ['4','6', '8', 'diploma'];
 
 subjects.forEach(subject => {
-    const periods = subjects == "math" || subjects == "cz" ? allPeriods: ['diploma']
+    const periods = (subject == "math" || subject == "cz") ? allPeriods: ['diploma'];
     periods.forEach(period => {
         const cmd = `node ./src/cermat/pdf-[subject]-[period].json.js --subject ${subject} --period ${period}`;
         console.log(`\n▶ Running: ${cmd}`);
